f9558312b9b9c2ca2a1a03a6b908ade0f9f81820
p.e.visscher <p.e.visscher@zefiros.eu>
2018-07-09 20:20:01 +0200

    Add post extract and link

---
+++ .post-extract.lua
@@ -0,0 +1,32 @@
+-- [[
+-- @cond ___LICENSE___
+--
+-- Copyright (c) 2016-2018 Zefiros Software.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--
+-- @endcond
+-- ]]
+
+if os.istarget("windows") then
+    local temp = zpm.env.getTempDirectory()
+    local to = path.join(temp, 'package.zip')
+    http.download('https://www.nuget.org/api/v2/package/WinPixEventRuntime/1.0.180612001', to)
+    zpm.unzip(to, 'winpix')
+end
